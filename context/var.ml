77d945c7a7a69597768682a51f810d8c64c74886
Jordy Dickinson <jordy.dickinson@icloud.com>
2021-10-29 20:20:04 -0400

    Add Var.{indexed,leveled}

--- context/var.ml
+++ context/var.ml
@@ -1,3 +1,10 @@
+type index = Local.index
+type level = Local.level
+
 type (_, _) t =
 | Local : 'l Local.t -> ('l, _) t
 | Global : 'g Global.t -> (_, 'g) t
+
+type indexed = (index, index) t
+
+type leveled = (level, level) t
--- context/var.mli
+++ context/var.mli
@@ -1,4 +1,13 @@
+type index := Local.index
+type level := Local.level
+
 (** Local or global variables. *)
 type (_, _) t =
 | Local : 'l Local.t -> ('l, _) t
 | Global : 'g Global.t -> (_, 'g) t
+
+(** Indexed variables *)
+type indexed = (index, index) t
+
+(** Leveled variables *)
+type leveled = (level, level) t
