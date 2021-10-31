afc97907579e47f07120a3f49d593b0434771d2c
Jordy Dickinson <jordy.dickinson@icloud.com>
2021-10-31 13:26:50 -0400

    Add Name.Space

--- context/name.ml
+++ context/name.ml
@@ -1,5 +1,6 @@
 module Index = Local.Index
 module Level = Local.Level
+module Pool = Local.Pool
 
 type index = Index.t
 type level = Level.t
@@ -10,6 +11,8 @@ type _ t =
 
 type indexed = index t
 type leveled = level t
+
+type space = Local.pool Ident.Map.t
 
 let indexed id i = Indexed (id, i)
 
@@ -32,3 +35,41 @@ let equal (type a) (x: a t) (y: a t) : bool = match x, y with
 | Indexed _, _ -> false
 | Leveled (id, l), Leveled (id', l') -> Ident.equal id id' && Level.equal l l'
 | Leveled _, _ -> false
+
+module Space = struct
+  type t = space
+
+  let empty = Ident.Map.empty
+
+  let is_empty = Ident.Map.is_empty
+
+  let subspace sp1 sp2 = sp1 |> Ident.Map.for_all begin fun id pool1 ->
+    match Ident.Map.find_opt id sp2 with
+    | None -> false
+    | Some pool2 -> Pool.subpool pool1 pool2
+  end
+
+  let union = Ident.Map.union (fun _ pool1 pool2 -> Some (Pool.union pool1 pool2))
+
+  let pool id space =
+    Ident.Map.find_opt id space
+    |> Option.value ~default:Pool.empty
+
+  let extend id = Ident.Map.update id begin function
+  | None -> Some (Pool.extend Pool.empty)
+  | Some pool -> Some (Pool.extend pool)
+  end
+
+  let shrink id = Ident.Map.update id begin function
+  | None -> raise Not_found
+  | Some pool ->
+    let pool = Pool.shrink pool in
+    if Pool.is_empty pool then None else Some pool
+  end
+
+  let shrink_opt id space = try Some (shrink id space) with Not_found -> None
+
+  let mem name space = match Ident.Map.find_opt (ident name) space with
+  | None -> false
+  | Some pool -> Pool.mem (to_local name) pool
+end
--- context/name.mli
+++ context/name.mli
@@ -1,5 +1,6 @@
 type index := Local.index
 type level := Local.level
+type pool := Local.pool
 
 type _ t =
 | Indexed : Ident.t * index -> index t
@@ -10,6 +11,8 @@ include Equatable.S1 with type 'a t := 'a t
 type indexed = index t
 type leveled = level t
 
+type space
+
 val indexed : Ident.t -> index -> indexed
 
 val leveled : Ident.t -> level -> leveled
@@ -19,3 +22,42 @@ val ident : 'a t -> Ident.t
 val to_local : 'a t -> 'a Local.t
 
 val drop_ident : 'a t -> 'a
+
+module Space: sig
+  type 'a name := 'a t
+  type t = space
+
+  (** [empty] is the namespace with no names. *)
+  val empty: t
+
+  (** [is_empty space] is [true] if [space] is empty and [false] otherwise. *)
+  val is_empty: t -> bool
+
+  (** [subspace space1 space2] is [true] if all names in [space1] are also in
+      [space2]. *)
+  val subspace: t -> t -> bool
+
+  (** [union space1 space2] is a namespace containing all the names in both
+      [space1] and [space2]. *)
+  val union: t -> t -> t
+
+  (** [pool id space] is the pool of indices/levels associated with [id] in
+      [space]. If [space] contains no names identified by [id], this pool is
+      empty. *)
+  val pool: Ident.t -> t -> pool
+
+  (** [extend id space] is [space] with an additional name identified by [id]
+      at De Bruijn index 0. *)
+  val extend: Ident.t -> t -> t
+
+  (** [shrink id space] is [space] with the most-recently-added name identified
+      by [id] removed, or @raise Not_found if no such name exists. *)
+  val shrink: Ident.t -> t -> t
+
+  (** [shrink_opt] is like {!val:shrink} but it returns [None] rather than
+      raising an exception. *)
+  val shrink_opt: Ident.t -> t -> t option
+
+  (** [mem name space] is [true] if [name] is a member of [space]. *)
+  val mem: _ name -> t -> bool
+end
